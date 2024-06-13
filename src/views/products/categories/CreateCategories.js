b08d5e3ef924bd87b2d116d4d58eeb862cf9aaf0
KUNAL_SINGH1999 <kunal@codetoshine.com>
2024-06-13 17:04:48 +0530

    13 june 5:04pm

--- src/views/products/categories/CreateCategories.js
+++ src/views/products/categories/CreateCategories.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react'
 import {
   CButton,
   CModal,
@@ -14,31 +14,30 @@ import {
   CTableHeaderCell,
   CTableBody,
   CTableDataCell,
-  CSpinner
-} from '@coreui/react';
-import { useDropzone } from 'react-dropzone';
+  CSpinner,
+} from '@coreui/react'
+import { useDropzone } from 'react-dropzone'
 import CIcon from '@coreui/icons-react'
 import { cilPencil, cilTrash, cilCloudUpload } from '@coreui/icons'
 import '../Products.css' // Import custom CSS file
-import axiosInstance, { baseURL } from '../../../utils/axiosConfig';
-import getCategoryById from '../../../api/category/categoryapi';
-import updateCategory from '../../../api/category/updateCategory';
-import deleteCategory from '../../../api/category/deleteCategory';
-import { useDispatch, useSelector } from 'react-redux';
-import { startLoading, stopLoading } from '../../../store';
-
+import axiosInstance, { baseURL } from '../../../utils/axiosConfig'
+import getCategoryById from '../../../api/category/categoryapi'
+import updateCategory from '../../../api/category/updateCategory'
+import deleteCategory from '../../../api/category/deleteCategory'
+import { useDispatch, useSelector } from 'react-redux'
+import { startLoading, stopLoading } from '../../../store'
 
 const Categories = () => {
   const dispatch = useDispatch()
   const isLoading = useSelector((state) => state.loading)
-  const [categories, setCategories] = useState([]);
-  const [modal, setModal] = useState(false);
-  const [editingCategory, setEditingCategory] = useState(null);
-  const [form, setForm] = useState({ name: '', images: [] });
+  const [categories, setCategories] = useState([])
+  const [modal, setModal] = useState(false)
+  const [editingCategory, setEditingCategory] = useState(null)
+  const [form, setForm] = useState({ name: '', images: [] })
 
   useEffect(() => {
-    fetchCategories();
-  }, []);
+    fetchCategories()
+  }, [])
 
   const toggleModal = () => {
     if (editingCategory !== null) {
@@ -46,22 +45,22 @@ const Categories = () => {
       setEditingCategory(null)
     }
     setModal(!modal)
-  };
+  }
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
 
   const handleSubmit = async (categoryId) => {
     try {
       dispatch(startLoading())
       if (editingCategory !== null) {
-        const updatedCategories = categories.map((category, index) =>
-          index === editingCategory ? form : category
-        );
+        const updatedcategories = categories.map((category, index) =>
+          index === editingCategory ? form : category,
+        )
         // setCategories(updatedCategories);
-        await updateCategory(categoryId, form);
+        await updateCategory(categoryId, form)
         await fetchCategories()
         dispatch(stopLoading())
       } else {
@@ -69,102 +68,100 @@ const Categories = () => {
         dispatch(stopLoading())
         // setCategories([...categories, form]);
       }
-      setForm({ name: '', images: [] });
-      setEditingCategory(null);
-      toggleModal();
+      setForm({ name: '', images: [] })
+      setEditingCategory(null)
+      toggleModal()
     } catch (error) {
       dispatch(stopLoading())
-      console.log("error", error)
+      console.log('error', error)
     }
-   
-  };
-
-
+  }
 
   const handleDelete = async (id) => {
     try {
-      await deleteCategory(id);
+      await deleteCategory(id)
       fetchCategories()
     } catch (error) {
-      console.error('Failed to delete category:', error);
+      console.error('Failed to delete category:', error)
     }
-  };
+  }
 
-  const onDrop = useCallback(acceptedFiles => {
-    // Update form state with the uploaded images
-    setForm({ ...form, images: form.images.concat(acceptedFiles) });
-  }, [form]);
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      // Update form state with the uploaded images
+      setForm({ ...form, images: form.images.concat(acceptedFiles) })
+    },
+    [form],
+  )
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
   const removeImage = (index) => {
     setForm({
       ...form,
-      images: form.images.filter((_, i) => i !== index)
-    });
-  };
+      images: form.images.filter((_, i) => i !== index),
+    })
+  }
 
   const saveToDb = async () => {
     dispatch(startLoading())
-    const formData = new FormData();
-    formData.append('name', form.name);
-    form.images.forEach(image => {
-      formData.append('images', image);
-    });
+    const formData = new FormData()
+    formData.append('name', form.name)
+    form.images.forEach((image) => {
+      formData.append('images', image)
+    })
 
     try {
       const response = await axiosInstance.post('category', formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
+          'Content-Type': 'multipart/form-data',
+        },
+      })
       await fetchCategories()
       dispatch(stopLoading())
     } catch (error) {
       dispatch(stopLoading())
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   const fetchCategories = async () => {
     try {
       dispatch(startLoading())
-      const response = await axiosInstance.get('category'); // Adjust the URL as necessary
+      const response = await axiosInstance.get('category') // Adjust the URL as necessary
       // const data = await response.json();
       if (response.status === 200) {
-        console.log("response.categories====>>", response.data.categories)
-        setCategories(response.data.categories);
+        console.log('response.categories====>>', response.data.categories)
+        setCategories(response.data.categories)
         dispatch(stopLoading())
       } else {
         // console.error(data.message);
       }
     } catch (error) {
       dispatch(stopLoading())
-      console.error('Failed to fetch categories', error);
+      console.error('Failed to fetch categories', error)
     }
-  };
+  }
 
   const handleEdit = async (index, id) => {
     let singCategory = await fetchCategoryById(id)
-    console.log("single category--->>>", singCategory)
-    setEditingCategory(index);
+    console.log('single category--->>>', singCategory)
+    setEditingCategory(index)
     // setForm(categories[index]);
     setForm(singCategory.category)
-    toggleModal();
-  };
-
+    toggleModal()
+  }
 
   const fetchCategoryById = async (categoryId) => {
     try {
-      const data = await getCategoryById(categoryId);
-      console.log('Category retrieved successfully:', data);
+      const data = await getCategoryById(categoryId)
+      console.log('Category retrieved successfully:', data)
       return data
       // Handle the retrieved category data as needed
     } catch (error) {
-      console.error('Error fetching category:', error);
+      console.error('Error fetching category:', error)
     }
-  };
-
+  }
 
   return (
     <div>
@@ -174,9 +171,11 @@ const Categories = () => {
           Add Category
         </CButton>
       </div>
-      {isLoading ? <div className="spinner-container">
-        <CSpinner size="sm" color="blue" />
-      </div> :
+      {isLoading ? (
+        <div className="spinner-container">
+          <CSpinner size="sm" color="blue" />
+        </div>
+      ) : (
         <CTable striped>
           <CTableHead>
             <CTableRow>
@@ -199,8 +198,8 @@ const Categories = () => {
                   ))}
                 </CTableDataCell>
                 <CTableDataCell>{category?.name}</CTableDataCell>
-                <CTableDataCell >
-                  <div className='actions-cell'>
+                <CTableDataCell>
+                  <div className="actions-cell">
                     <CButton color="warning" onClick={() => handleEdit(index, category?._id)}>
                       <CIcon icon={cilPencil} />
                     </CButton>{' '}
@@ -208,12 +207,12 @@ const Categories = () => {
                       <CIcon icon={cilTrash} />
                     </CButton>
                   </div>
-
                 </CTableDataCell>
               </CTableRow>
             ))}
           </CTableBody>
-        </CTable>}
+        </CTable>
+      )}
 
       <CModal visible={modal} onClose={toggleModal}>
         <CModalHeader>
@@ -241,25 +240,34 @@ const Categories = () => {
                 <div key={index} className="image-wrapper">
                   <img
                     className="img"
-                    src={typeof file === 'string' ? `${baseURL}/${file}` : URL.createObjectURL(file)}
+                    src={
+                      typeof file === 'string' ? `${baseURL}/${file}` : URL.createObjectURL(file)
+                    }
                     alt={`Category Image ${index + 1}`}
                   />
-                  <button type="button" className="close-button" onClick={() => removeImage(index)}>✖</button>
+                  <button type="button" className="close-button" onClick={() => removeImage(index)}>
+                    ✖
+                  </button>
                 </div>
               ))}
             </div>
           </CForm>
-
         </CModalBody>
         <CModalFooter>
-        {isLoading ? <CButton color="primary">{editingCategory !== null ? "Updating Category" : "Creating Category"} <CSpinner size="sm" color='white' /></CButton> : <CButton color="primary" onClick={() => handleSubmit(form._id)}>
-            {editingCategory !== null ? 'Save Changes' : 'Add Category'}
-          </CButton>}
-          
+          {isLoading ? (
+            <CButton color="primary">
+              {editingCategory !== null ? 'Updating Category' : 'Creating Category'}{' '}
+              <CSpinner size="sm" color="white" />
+            </CButton>
+          ) : (
+            <CButton color="primary" onClick={() => handleSubmit(form._id)}>
+              {editingCategory !== null ? 'Save Changes' : 'Add Category'}
+            </CButton>
+          )}
         </CModalFooter>
       </CModal>
     </div>
-  );
-};
+  )
+}
 
-export default Categories;
+export default Categories
