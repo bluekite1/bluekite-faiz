5a69266db6ed7ea88f1c066f3e2ccdd306a8b5ea
waseemAnsari11011 <waseemahm11011@gmail.com>
2024-08-04 15:22:42 +0530

    abc

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000';
+// const baseURL = 'http://localhost:8000';
+const baseURL = 'https://server.bluekitestore.com';
 
 const axiosInstance = axios.create({
   baseURL: baseURL, // Base URL of your API
